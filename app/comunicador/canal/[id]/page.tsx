d60269f40674410d3567588d1b1b125639eb9a30
Devaldo Nascimento <devaldo.nas@gmail.com>
2026-05-15 17:39:40 -0300

    chore: trigger deploy

--- app/comunicador/canal/[id]/page.tsx
+++ app/comunicador/canal/[id]/page.tsx
@@ -243,7 +243,7 @@ export default function SalaComunicador() {
       </div>
     )
   }
-
+//novo
   return (
     <div className="min-h-screen bg-gray-50 pb-20">
       <div className="max-w-2xl mx-auto px-4 py-8">
